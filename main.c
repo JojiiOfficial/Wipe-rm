e3bf9839d70431066bef3d23d23c4a750f5e3bc0
jojiiOfficial <jojii@jojii.de>
2020-01-16 19:55:13 +0100

    added program args

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <dirent.h>
 #include <stdlib.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 int isDirectory(const char *path) {
     struct stat statbuf;
@@ -14,14 +15,22 @@ int isDirectory(const char *path) {
     return S_ISDIR(statbuf.st_mode);
 }
 
-bool deleteFile(const char *file){
+bool delete(const char *file, bool recursive, bool quiet, bool verbose, bool force, bool zeroOnly){
     if (!isDirectory(file)){
         FILE *f;
         if (!(f = fopen(file, "r+"))) {
-            printf("Error opening file: %s\n",file);
+            printf("Error opening file: '%s'\n",file);
+            if (verbose){
+                printf("Use -f to skip errors\n");
+            }
+            if (!force){
+                exit(EXIT_FAILURE);
+            }
             return false;
         }
-        printf("deleting file: %s\n", file);
+        if (verbose){
+            printf("deleting file: %s\n", file);
+        }
         fseek(f, 0, SEEK_END);
         long int size = ftell(f);
         fseek(f, 0, SEEK_SET);
@@ -49,7 +58,7 @@ bool deleteFile(const char *file){
             strcat(path, file);
             strcat(path, "/");
             strcat(path, de->d_name);
-            deleteFile(path);
+            delete(path, recursive, quiet, verbose, force, zeroOnly);
             free(path);
 
             i++;
@@ -62,6 +71,32 @@ bool deleteFile(const char *file){
     return false;
 }
 
-void main(){
-    deleteFile("test");
+void usage(char* s, short exitcode){
+    printf("Usage: %s [-hrfqv] [file...]\n",s);
+    exit(exitcode);
+}
+
+void main(int argc, char** argv){
+    int opt;
+    bool recursive, force, verbose, quiet, zeroOnly;
+
+    while ((opt = getopt(argc, argv, "rfqvhz")) != -1) {
+        switch (opt) {
+        case 'r': recursive = true; break;
+        case 'f': force = true; break;
+        case 'q': quiet = true; break;
+        case 'v': verbose = true; break;
+        case 'z': zeroOnly = true; break;
+        case 'h': usage(argv[0],0); break;
+        default:
+            usage(argv[0], EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (!strncmp(argv[i], "-",1)){
+            continue;
+        }
+        delete(argv[i], recursive, quiet, verbose, force, zeroOnly);
+    }
 }
